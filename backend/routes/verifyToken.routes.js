7146c9c12cdd693126ee41e72984beb236a9b17b
Poulami Adhikary <poulamiadhikary18@gmail.com>
2025-04-19 16:32:15 +0530

    Delete backend directory

--- backend/routes/verifyToken.routes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { verifyToken } from '../controllers/verifyToken.js';
-
-const router = express.Router();
-
-router.get('/verify-token', verifyToken);
-
-export default router;
